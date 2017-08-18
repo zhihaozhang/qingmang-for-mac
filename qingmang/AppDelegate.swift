76baf081dfb643e2d6ece4888a32c766ee68ef95
Chih-Hao <zhihaozhang@me.com>
2017-08-18 15:32:21 +0800

    Initial Commit

---
+++ qingmang/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  qingmang
+//
+//  Created by Chih-Hao on 2017/8/18.
+//  Copyright © 2017年 Chih-Hao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ qingmang/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  qingmang
+//
+//  Created by Chih-Hao on 2017/8/18.
+//  Copyright © 2017年 Chih-Hao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
