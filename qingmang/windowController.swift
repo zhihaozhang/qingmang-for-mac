73da27b5d2fb7969a546de5c72449893ffe8f889
Chih-Hao <zhihaozhang@me.com>
2017-08-19 17:11:02 +0800

    title-bar transparent

--- qingmang/windowController.swift
+++ qingmang/windowController.swift
@@ -15,6 +15,8 @@ class windowController: NSWindowController {
     
         window?.titleVisibility = .hidden
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        window?.titlebarAppearsTransparent = true
+        
     }
 
 }
