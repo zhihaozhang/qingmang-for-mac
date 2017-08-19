4453087d76ad6cc948d1b61934f79d5da025947c
Chih-Hao <zhihaozhang@me.com>
2017-08-19 17:20:06 +0800

    样式定稿

--- qingmang/windowController.swift
+++ qingmang/windowController.swift
@@ -15,6 +15,7 @@ class windowController: NSWindowController {
     
         window?.titleVisibility = .hidden
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        window?.backgroundColor = NSColor.white
         window?.titlebarAppearsTransparent = true
         
     }
