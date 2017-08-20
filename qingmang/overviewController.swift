e99a9f41e371ccce808fce6296c0f2c255bc57f5
Chih-Hao <zhihaozhang@me.com>
2017-08-20 12:10:37 +0800

    侧边栏按钮增加了选中/非选中状态

--- qingmang/overviewController.swift
+++ qingmang/overviewController.swift
@@ -10,43 +10,68 @@ import Cocoa
 
 class overviewController: NSViewController,NSTableViewDelegate,NSTableViewDataSource {
     
+    @IBOutlet var firstPageButton: NSButton!
+    
+    var highlightNow: NSButton? = nil
+    
     @IBOutlet var tableview: NSTableView!
     
     @IBAction func firstPage(_ sender: Any) {
         changeTheme("p2557")
+        changeHighlight(sender)
     }
     @IBAction func people(_ sender: Any) {
         changeTheme("p3023")
+        changeHighlight(sender)
     }
     @IBAction func news(_ sender: Any) {
          changeTheme("p3029")
+        changeHighlight(sender)
     }
     @IBAction func atitude(_ sender: Any) {
         changeTheme("p4318")
+        changeHighlight(sender)
     }
     @IBAction func duanzi(_ sender: Any) {
         changeTheme("p4320")
+        changeHighlight(sender)
     }
     @IBAction func knowledge(_ sender: Any) {
         changeTheme("p4322")
+        changeHighlight(sender)
     }
     @IBAction func product(_ sender: Any) {
         changeTheme("p4324")
+        changeHighlight(sender)
     }
     @IBAction func productClicked(_ sender: Any) {
         changeTheme("p4326")
+        changeHighlight(sender)
     }
     @IBAction func travel(_ sender: Any) {
         changeTheme("i4928")
+        changeHighlight(sender)
     }
     @IBAction func google(_ sender: Any) {
         changeTheme("i6280")
+        changeHighlight(sender)
     }
  
     @IBAction func catClicked(_ sender: Any) {
         changeTheme("p2451")
+        changeHighlight(sender)
     }
     
+    func changeHighlight(_ sender:Any){
+        
+        highlightNow?.state = NSOffState
+        var sender1 = sender as! NSButton
+        sender1.state = NSOnState
+        highlightNow = sender1
+    }
+    
+    
+    @IBOutlet var categoryView: NSView!
     
     
     var feed : JSON?
@@ -91,6 +116,12 @@ class overviewController: NSViewController,NSTableViewDelegate,NSTableViewDataSo
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        highlightNow = firstPageButton
+        
+        categoryView.wantsLayer = true
+        categoryView.layer?.backgroundColor = NSColor(red: 112/255, green: 128/255, blue: 144/255, alpha: 0.1).cgColor
+        
         var dataSource = "https://api.qingmang.me/v2/article.list?token=92f136746dd34370a71363f6b66a3e01&category_id=p2557"
         
         guard let url = NSURL(string: dataSource) else{return }
@@ -110,7 +141,7 @@ class overviewController: NSViewController,NSTableViewDelegate,NSTableViewDataSo
         
         // Do view setup here.
     }
-    
+   
     func numberOfRows(in tableView: NSTableView) -> Int {
         guard let feed = self.feed else {
             return 0
--- qingmang/windowController.swift
+++ qingmang/windowController.swift
@@ -15,7 +15,7 @@ class windowController: NSWindowController {
     
         window?.titleVisibility = .hidden
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
-//        window?.backgroundColor = NSColor.white
+        window?.backgroundColor = NSColor.white
         
         
         self.window?.titleVisibility = .hidden
