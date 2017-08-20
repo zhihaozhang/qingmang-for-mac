3cad1caff4f168919fc470f527d502e6c1ace65d
Chih-Hao <zhihaozhang@me.com>
2017-08-20 11:12:53 +0800

    移除title-bar 最大化最小化关闭按钮保留

--- qingmang/windowController.swift
+++ qingmang/windowController.swift
@@ -15,8 +15,13 @@ class windowController: NSWindowController {
     
         window?.titleVisibility = .hidden
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
-        window?.backgroundColor = NSColor.white
-        window?.titlebarAppearsTransparent = true
+//        window?.backgroundColor = NSColor.white
+        
+        
+        self.window?.titleVisibility = .hidden
+        self.window?.titlebarAppearsTransparent = true
+        self.window?.styleMask.insert(.fullSizeContentView)
+        
         
     }
 
