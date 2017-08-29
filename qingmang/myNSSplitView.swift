486803b9937974e1d3ffe9f1ac374ca628cb0662
Chih-Hao <zhihaozhang@me.com>
2017-08-29 16:52:44 +0800

    accept issue#2

---
+++ qingmang/myNSSplitView.swift
@@ -0,0 +1,18 @@
+//
+//  myNSSplitView.swift
+//  qingmang
+//
+//  Created by Chih-Hao on 2017/8/29.
+//  Copyright © 2017年 Chih-Hao. All rights reserved.
+//
+
+import Cocoa
+
+class myNSSplitView: NSSplitView {
+
+    
+    override var dividerThickness: CGFloat {
+        get { return 1}
+    }
+    
+}
